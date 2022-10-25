7abc833bcf8a32fa83d48499485eb9759a27428c
Jacob Lewallen <jlewallen@gmail.com>
2022-10-24 17:28:43 -0700

    Add detail to error, doesn't seem to be working.

--- native/scrap/src/lib.rs
+++ native/scrap/src/lib.rs
@@ -1,26 +1,32 @@
 use std::{error, fmt, io};
 
 /// A useless Error just for the Demo
-#[derive(Copy, Clone, Debug)]
-pub struct ScrapError;
+#[derive(Clone, Debug)]
+pub struct ScrapError {
+    message: String,
+}
 
 impl fmt::Display for ScrapError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Error While Scrapping this page.")
+        write!(f, "scrape error: {}", self.message)
     }
 }
 
 impl error::Error for ScrapError {}
 
 impl From<reqwest::Error> for ScrapError {
-    fn from(_: reqwest::Error) -> Self {
-        Self
+    fn from(e: reqwest::Error) -> Self {
+        Self {
+            message: e.to_string(),
+        }
     }
 }
 
 impl From<io::Error> for ScrapError {
-    fn from(_: io::Error) -> Self {
-        Self
+    fn from(e: io::Error) -> Self {
+        Self {
+            message: e.to_string(),
+        }
     }
 }
 
